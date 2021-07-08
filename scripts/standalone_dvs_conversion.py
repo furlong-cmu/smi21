9cc0a395038c6e3472cb08903cb8444b6cca5430
Michael Furlong <furlong@gmail.com>
2021-07-08 20:46:28 +0200

    Fixed bug in event image, downsampled the images to speed up throughput

--- scripts/standalone_dvs_conversion.py
+++ scripts/standalone_dvs_conversion.py
@@ -14,6 +14,7 @@ from sensor_msgs.msg import Image
 from v2ecore.emulator import EventEmulator
 
 import util
+import time
 
 class StandaloneDVS:
     
@@ -26,12 +27,12 @@ class StandaloneDVS:
         if self.real_generator:
             rospy.loginfo('Creating Event Emulator')
             self.emulator = EventEmulator(
-                   pos_thres=0.2,
-                   neg_thres=0.2,
-                   sigma_thres=0.03,
-                   cutoff_hz=200,
-                   leak_rate_hz=1,
-                   shot_noise_rate_hz=10,
+                   pos_thres=0.5,
+                   neg_thres=0.5,
+                   sigma_thres=0.025,
+                   cutoff_hz=0,
+                   leak_rate_hz=0,
+                   shot_noise_rate_hz=0,
                    device="cpu")
             rospy.loginfo('Emulator created')
         ### end if
@@ -40,23 +41,28 @@ class StandaloneDVS:
         self.sub = rospy.Subscriber('/husky/husky/camera',
                                     Image,
                                     self.camera_callback)
-        self.pub = rospy.Publisher('/smi21/event_image', Image, queue_size=1)
+        self.pub = rospy.Publisher('/smi21/event_image', Image,
+                                    queue_size=1)
         self.image = None
         self.num_channels = {'rgb8':3}
 	
     def camera_callback(self, img_msg):
 
-        # TODO:Make sure colour conversion and reshaping works properly
         #rospy.loginfo('Image message received')
         colour_img = np.frombuffer(img_msg.data, np.uint8).reshape((img_msg.height, img_msg.width, self.num_channels[img_msg.encoding]))
 
+        num_downsamples = 1 # time between 1 and 2 doesn't seem huge?
         self.image = cv2.cvtColor(colour_img, cv2.COLOR_RGB2GRAY)
+        for _ in range(num_downsamples): 
+            self.image = cv2.pyrDown(self.image)
         self.time = img_msg.header.stamp.to_sec()
 
         if self.real_generator:
             rospy.loginfo('Generating events')
+            start = time.time()
             self.events = self.emulator.generate_events(self.image, self.time)
-            rospy.loginfo('Events generated')
+            dt = time.time() - start
+            rospy.loginfo(f'Events generated in {dt} seconds')
         else:
             self.events = util.random_events(self.image.shape, self.time) 
         ### end if
@@ -64,12 +70,12 @@ class StandaloneDVS:
         zero_channel = np.zeros((event_frame.shape[0], event_frame.shape[1], 1),
                                 dtype=np.uint8)
 
-        output = np.concatenate((event_frame, zero_channel), axis=-1).astype(np.uint8)
-        msg = Image(height=img_msg.height,
-                    width=img_msg.width,
+        output = np.concatenate((50 * event_frame, zero_channel), axis=-1).astype(np.uint8)
+        msg = Image(height=img_msg.height // 2**num_downsamples,
+                    width=img_msg.width // 2**num_downsamples,
                     encoding=img_msg.encoding,
                     is_bigendian=img_msg.is_bigendian,
-                    step=img_msg.step,
+                    step=img_msg.step//2**num_downsamples,
                     data = output.flatten().tolist())
         msg.header.stamp = rospy.Time.now()
         self.pub.publish(msg)
--- scripts/util.py
+++ scripts/util.py
@@ -13,7 +13,7 @@ def to_event_frame(events, image_shape):
     retval = np.zeros((image_shape[0], image_shape[1], 2), dtype=np.uint8)
     for e in events:
         # TODO: Confirm that x -> width and y-> height
-        retval[int(e[2]),int(e[1]),int(e[3])] += 1
+        retval[int(e[2]),int(e[1]),int(e[3] > 0)] += 1
     return retval
 
 def random_events(image_shape, time):
