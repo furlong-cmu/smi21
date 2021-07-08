4e5247915bd797448d90c66a2aa5fbfacb1f66d2
Michael Furlong <furlong@gmail.com>
2021-07-08 16:49:25 +0200

    Refactored out random event generator for testing data pipeline

--- scripts/standalone_dvs_conversion.py
+++ scripts/standalone_dvs_conversion.py
@@ -21,7 +21,7 @@ class StandaloneDVS:
         rospy.init_node(module_name, anonymous=True)
 	# Create the event camera emulator.  
 	## TODO: Configurate the parameters.
-        self.real_generator = False
+        self.real_generator = True 
         self.emulator = None
         if self.real_generator:
             rospy.loginfo('Creating Event Emulator')
@@ -33,15 +33,14 @@ class StandaloneDVS:
                    leak_rate_hz=1,
                    shot_noise_rate_hz=10,
                    device="cpu")
+            rospy.loginfo('Emulator created')
         ### end if
         self.events = None
 
         self.sub = rospy.Subscriber('/husky/husky/camera',
                                     Image,
                                     self.camera_callback)
-#        self.pub = rospy.Publisher('/smi21/dvs_event_array', EventArray, queue_size=1)
         self.pub = rospy.Publisher('/smi21/event_image', Image, queue_size=1)
-#        rospy.Timer(rospy.Duration(0.1), self.send_event)
         self.image = None
         self.num_channels = {'rgb8':3}
 	
@@ -55,14 +54,11 @@ class StandaloneDVS:
         self.time = img_msg.header.stamp.to_sec()
 
         if self.real_generator:
+            rospy.loginfo('Generating events')
             self.events = self.emulator.generate_events(self.image, self.time)
+            rospy.loginfo('Events generated')
         else:
-            num_events = np.random.randint(10)
-            ts = np.ones((num_events, 1)) * self.time
-            xs = np.random.randint(low=0,high=self.image.shape[1], size=(num_events,1)).astype('uint16')
-            ys = np.random.randint(low=0,high=self.image.shape[0], size=(num_events,1)).astype('uint16')
-            ps = (np.random.random((num_events,1)) > 0.5).astype('bool')
-            self.events = np.hstack((ts,xs,ys,ps))
+            self.events = util.random_events(self.image.shape, self.time) 
         ### end if
         event_frame = util.to_event_frame(self.events, self.image.shape)
         zero_channel = np.zeros((event_frame.shape[0], event_frame.shape[1], 1),
@@ -72,37 +68,12 @@ class StandaloneDVS:
         msg = Image(height=img_msg.height,
                     width=img_msg.width,
                     encoding=img_msg.encoding,
+                    is_bigendian=img_msg.is_bigendian,
+                    step=img_msg.step,
                     data = output.flatten().tolist())
         msg.header.stamp = rospy.Time.now()
         self.pub.publish(msg)
         #rospy.loginfo('Event Image published')
-
-
-
-
-
-
-    def send_event(self, timer_event):
-
-	# Because producing events from the first image takes much
-	# longer than subsequent images, we don't publish events
-	# until we've done the first image, so people won't be getting
-	# empty event messages that don't mean anything.
-	# (unless we want to add some random noise or something?)
-        if not (self.events is None):
-            # Events are an N x 4 matrix of (time, x, y, polarity)
-
-            msg = EventArray()
-            msg.header.stamp = timer_event.current_real
-            msg.height = self.image.shape[0]
-            msg.width = self.image.shape[1]
-            msg.events = [Event(ts=rospy.Time.from_sec(x[0]), x=x[1].astype('uint16'), y=x[2].astype('uint16'), polarity=x[3].astype('bool')) for x in self.events]
-
-            self.pub.publish(msg)
-
-            # Remove the just processed events.
-            ## HACK: Make sure this isn't stupid (re: concurrency) later.
-            self.events = None
 
 if __name__ == "__main__":
     m = StandaloneDVS(module_name='dvs_sim')
