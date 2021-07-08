c41beeaf1f919e8349e702334096c4d398d72af6
Michael Furlong <furlong@gmail.com>
2021-07-08 16:49:19 +0200

    Refactored out random event generator for testing data pipeline

--- scripts/util.py
+++ scripts/util.py
@@ -15,3 +15,12 @@ def to_event_frame(events, image_shape):
         # TODO: Confirm that x -> width and y-> height
         retval[int(e[2]),int(e[1]),int(e[3])] += 1
     return retval
+
+def random_events(image_shape, time):
+
+    num_events = np.random.randint(10)
+    ts = np.ones((num_events, 1)) * time
+    xs = np.random.randint(low=0,high=image_shape[1], size=(num_events,1)).astype('uint16')
+    ys = np.random.randint(low=0,high=image_shape[0], size=(num_events,1)).astype('uint16')
+    ps = (np.random.random((num_events,1)) > 0.5).astype('bool')
+    return np.hstack((ts,xs,ys,ps))
